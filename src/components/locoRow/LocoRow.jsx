4f26b9abcd165b487c6fc4c7288f14c57f813601
xorua117 <xorua117@gmail.com>
2021-11-24 16:21:22 +0900

    Render item based on tags

--- src/components/locoRow/LocoRow.jsx
+++ src/components/locoRow/LocoRow.jsx
@@ -5,8 +5,12 @@ import RowContainer from './RowContainer';
 
 import { webtoon } from '../../resources/data';
 
-function LocoRow({ title }) {
-  const data = webtoon.slice(0);
+function LocoRow({ title, type }) {
+  let data = webtoon.slice(0);
+
+  if (type) {
+    data = data.filter((data) => data.tags.find((tag) => tag === type));
+  }
 
   return (
     <LocoRowBlock>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,6 +5,8 @@ import Footer from '../../components/footer/Footer';
 import firebase from '../../api/firebase';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+import { webtoon } from '../../resources/data';
+
 function HomePage() {
   const auth = getAuth(firebase);
 
@@ -22,6 +24,18 @@ function HomePage() {
     }
   });
 
+  let data = webtoon.slice(0);
+
+  let allType = [];
+
+  data.forEach((data) =>
+    data.tags.forEach((tag) => {
+      allType.push(tag);
+    })
+  );
+
+  allType = Array.from(new Set(allType));
+
   return (
     <div>
       <Header />
@@ -30,14 +44,9 @@ function HomePage() {
       <LocoRow title="오늘의 한국의 TOP 10 콘텐츠" />
       <LocoRow title="한국이 만든 콘텐츠" />
       <LocoRow title="이태겸님이 감상중인 콘텐츠" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 22" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 333" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 4444" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 55555" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 666666" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 7777777" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 88888888" />
-      <LocoRow title="이태겸님이 감상중인 콘텐츠 999999999" />
+      {allType.map((type) => (
+        <LocoRow title={type} type={type} />
+      ))}
       <Footer />
     </div>
   );
